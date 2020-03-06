f27192eb209206f385e41e61ead023df62e26fc0
Raul Leandro <raul_leandro@venalum.com.ve>
2020-03-06 08:19:20 -0400

    modificacion de configuracion de plantilla para que aparezca nombre del proyecyto y de la base de datos para la conexion

--- config/adminlte.php
+++ config/adminlte.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'title' => 'AdminLTE 3',
+    'title' => 'SICER',
     'title_prefix' => '',
     'title_postfix' => '',
 
@@ -45,7 +45,7 @@ return [
     |
     */
 
-    'logo' => '<b>Admin</b>LTE',
+    'logo' => '<b>SICER</b>',
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image-xl',
     'logo_img_xl' => null,
